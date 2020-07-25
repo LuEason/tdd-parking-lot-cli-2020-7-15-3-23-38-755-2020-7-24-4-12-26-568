61f3b6d9c59278fdc6a2d05523fa355cf55bfc58
Eason <chenzscd@163.com>
2020-07-25 12:25:40 +0800

    [parkingLot][Eason] feature: add should_fetch_car_when_fetch_given_car_ticket test

--- src/main/java/com/oocl/cultivation/ParkingBoy.java
+++ src/main/java/com/oocl/cultivation/ParkingBoy.java
@@ -2,6 +2,10 @@ package com.oocl.cultivation;
 
 public class ParkingBoy {
     public CarTicket park(Car car) {
+        return new CarTicket();
+    }
+
+    public Car fetch(CarTicket carTicket) {
         return null;
     }
 }
--- src/test/java/com/oocl/cultivation/test/ParkingBoyTest.java
+++ src/test/java/com/oocl/cultivation/test/ParkingBoyTest.java
@@ -3,21 +3,39 @@ package com.oocl.cultivation.test;
 import com.oocl.cultivation.Car;
 import com.oocl.cultivation.CarTicket;
 import com.oocl.cultivation.ParkingBoy;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 class ParkingBoyTest {
+    private ParkingBoy parkingBoy;
+
+    @BeforeEach
+    public void setup() {
+        parkingBoy = new ParkingBoy();
+    }
+
     @Test
     void should_return_CarTicket_when_park_given_car() {
         //given
         Car car = new Car();
-        ParkingBoy parkingBoy = new ParkingBoy();
 
         //when
         CarTicket carTicket = parkingBoy.park(car);
 
         //then
         assertNotNull(carTicket);
+    }
+    @Test
+    void should_fetch_car_when_fetch_given_car_ticket() {
+        //given
+        CarTicket carTicket = new CarTicket();
+
+        //when
+        Car car = parkingBoy.fetch(carTicket);
+
+        //then
+        assertNotNull(car);
     }
 }
