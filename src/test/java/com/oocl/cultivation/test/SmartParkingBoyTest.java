b18d32056ba828cf442ec4b12df1e9b7c9572088
Eason <chenzscd@163.com>
2020-07-25 16:10:46 +0800

    [parkingLot][Eason] test: rename should_park_the_car_in_the_second_parking_lot_and_return_car_ticket_when_the_first_park_capacity_is_1_and_the_second_park_capacity_is_10_given_car to should_park_the_car_in_the_second_parking_lot_and_return_car_ticket_when_the_second_park_have_larger_position_given_car

--- src/test/java/com/oocl/cultivation/test/SmartParkingBoyTest.java
+++ src/test/java/com/oocl/cultivation/test/SmartParkingBoyTest.java
@@ -10,17 +10,16 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class SmartParkingBoyTest {
-    private ParkingBoy smartParkingBoy;
 
     @Test
-    void should_park_the_car_in_the_second_parking_lot_and_return_car_ticket_when_the_first_park_capacity_is_1_and_the_second_park_capacity_is_10_given_car() {
+    void should_park_the_car_in_the_second_parking_lot_and_return_car_ticket_when_the_second_park_have_larger_position_given_car() {
         //given
         List<ParkingLot> parkingLots = new ArrayList<>();
         ParkingLot firstParkingLot = new ParkingLot(1);
         parkingLots.add(firstParkingLot);
         ParkingLot secondParkingLot = new ParkingLot();
         parkingLots.add(secondParkingLot);
-        smartParkingBoy = new SmartParkingBoy(parkingLots);
+        ParkingBoy smartParkingBoy = new SmartParkingBoy(parkingLots);
         Car car = new Car();
 
         //when
