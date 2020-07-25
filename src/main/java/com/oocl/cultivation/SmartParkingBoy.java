7a6fef8cc96590008e3137429f701f295c27b827
Eason <chenzscd@163.com>
2020-07-25 16:04:42 +0800

    [parkingLot][Eason] feature: implement should_park_the_car_in_the_second_parking_lot_and_return_car_ticket_when_the_first_park_capacity_is_1_and_the_second_park_capacity_is_10_given_car

--- src/main/java/com/oocl/cultivation/SmartParkingBoy.java
+++ src/main/java/com/oocl/cultivation/SmartParkingBoy.java
@@ -6,4 +6,16 @@ public class SmartParkingBoy extends ParkingBoy {
     public SmartParkingBoy(List<ParkingLot> parkingLots) {
         super(parkingLots);
     }
+
+    public CarTicket park(Car car) {
+        int mostEmptyPositionsLotIndex = 0;
+        int mostEmptyPositions = parkingLots.get(0).countEmptyPosition();
+        for (ParkingLot parkingLot : parkingLots) {
+            if (parkingLot.countEmptyPosition() > mostEmptyPositions) {
+                mostEmptyPositionsLotIndex = parkingLots.indexOf(parkingLot);
+                mostEmptyPositions = parkingLot.countEmptyPosition();
+            }
+        }
+        return parkingLots.get(mostEmptyPositionsLotIndex).parking(car);
+    }
 }
